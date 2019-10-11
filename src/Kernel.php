bfc11fa80cbdb71aeef96afafd5b7c200c37a41e
Lars Steen <lats@aarhus.dk>
2019-10-11 13:45:12 +0200

    ODAA-61: Applying coding standards

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of itk-dev/odaa.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
