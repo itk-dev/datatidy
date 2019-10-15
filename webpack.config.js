420205c41dcc9af5de1d499cad5bafed4870cf9b
Lars Steen <lats@aarhus.dk>
2019-10-15 15:46:47 +0200

    ODAA-66: Making templates more simple

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,6 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('layout', './assets/js/layout.js')
-    .addEntry('login', './assets/js/login.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
