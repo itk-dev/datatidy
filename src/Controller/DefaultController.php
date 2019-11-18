d2711a4b0e4d37a4c9939af6b5e723ac5b018935
Mikkel Ricky <rimi@aarhus.dk>
2019-11-18 20:49:59 +0100

    ODAA-98: Added default controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of itk-dev/datatidy.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="frontpage")
+     */
+    public function index()
+    {
+        return $this->redirectToRoute('dashboard_index');
+    }
+}
