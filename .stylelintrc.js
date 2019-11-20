cb9e24141eb7819a73ef62f955542f6ea5b1a0a8
Jesper Pedersen <jesper@yepz.dk>
2019-11-20 12:46:22 +0100

    ODAA-37: Update stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
     "extends": "stylelint-config-recommended-scss",
     "rules": {
-        // "color-no-hex": [true, {
-        //     "message": "Don't use hex value for colors. Use predefined colors in _bootstrap-custom.scss"
-        // }],
+        "color-no-hex": [true, {
+            "message": "Don't use hex value for colors. Use predefined color-variables or add new in base.scss"
+        }],
         "color-named": ["never", {
-            "message": "Don't use named colors. Use predefined colors in _bootstrap-custom.scss"
+            "message": "Don't use named colors. Use predefined colors-variables"
         }],
         "number-leading-zero": ["never", {
             "message": "For consistency and to save a character don't use leading zeros on values less than 1"
@@ -20,6 +20,6 @@ module.exports = {
         }],
         "declaration-colon-space-after": "always",
         "no-duplicate-selectors": true,
-        "indentation": 2
+        "indentation": 4
     }
 }
