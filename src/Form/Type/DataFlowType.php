70ac799c3d7326804d5a93d604067e422c3bba2c
Mikkel Ricky <rimi@aarhus.dk>
2019-11-22 16:45:23 +0100

    Added form groups

--- src/Form/Type/DataFlowType.php
+++ src/Form/Type/DataFlowType.php
@@ -16,6 +16,7 @@ use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -26,23 +27,38 @@ class DataFlowType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
-            // @TODO: Use AJAX to load the list of users.
-            ->add('collaborators', EntityType::class, [
-                'class' => User::class,
-                'multiple' => true,
-                'expanded' => true,
-            ])
-            ->add('ttl', IntegerType::class)
-            ->add('dataSource', EntityType::class, [
-                'class' => DataSource::class,
-                'placeholder' => '',
-            ])
-            ->add('dataTargets', CollectionType::class, [
-                'entry_type' => DataTargetType::class,
-                'allow_add' => true,
-                'allow_delete' => true,
-            ])
+            ->add(
+                $builder->create('settings', FormType::class, [
+                    'inherit_data' => true,
+                ])
+                    ->add('name', TextType::class)
+                    // @TODO: Use AJAX to load the list of users.
+                    ->add('collaborators', EntityType::class, [
+                        'class' => User::class,
+                        'multiple' => true,
+                        'expanded' => true,
+                    ])
+            )
+            ->add(
+                $builder->create('data_source', FormType::class, [
+                        'inherit_data' => true,
+                ])
+                    ->add('ttl', IntegerType::class)
+                    ->add('dataSource', EntityType::class, [
+                        'class' => DataSource::class,
+                        'placeholder' => '',
+                    ])
+            )
+            ->add(
+                $builder->create('data_targets', FormType::class, [
+                        'inherit_data' => true,
+                ])
+                    ->add('dataTargets', CollectionType::class, [
+                        'entry_type' => DataTargetType::class,
+                        'allow_add' => true,
+                        'allow_delete' => true,
+                    ])
+            )
         ;
     }
 
