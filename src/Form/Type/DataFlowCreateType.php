6252cefc5f7f99871564b5e459a169d1978f7cb0
Mikkel Ricky <rimi@aarhus.dk>
2019-11-25 09:35:20 +0100

    ODAA-105: Updated create data flow step

---
+++ src/Form/Type/DataFlowCreateType.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of itk-dev/datatidy.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+namespace App\Form\Type;
+
+use App\Entity\DataSource;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class DataFlowCreateType extends DataFlowType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('dataSource', EntityType::class, [
+                'class' => DataSource::class,
+                'placeholder' => '',
+            ]);
+    }
+}
