7d09f87bab0c94b8359ee558c3616db2349cb543
Mikkel Ricky <rimi@aarhus.dk>
2019-12-02 14:32:23 +0100

    ODAA-130: Cleaned up after code review

--- src/Form/Type/DataFlowType.php
+++ src/Form/Type/DataFlowType.php
@@ -17,7 +17,6 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
