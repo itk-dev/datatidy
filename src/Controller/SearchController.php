db1cabc84aac111e4f5ba7a94b33d47d91af274e
Lars Steen <lats@aarhus.dk>
2020-05-26 10:20:12 +0200

    ODAA-180: Adding search functionality

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Repository\DataFlowJobRepository;
+use App\Repository\DataFlowRepository;
+use App\Repository\DataSourceRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/search")
+ */
+class SearchController extends AbstractController
+{
+    private $dataFlowRepository;
+    private $dataFlowJobRepository;
+    private $dataSourceRepository;
+
+    public function __construct(DataFlowRepository $dataFlowRepository, DataFlowJobRepository $dataFlowJobRepository, DataSourceRepository $dataSourceRepository)
+    {
+        $this->dataFlowRepository = $dataFlowRepository;
+        $this->dataFlowJobRepository = $dataFlowJobRepository;
+        $this->dataSourceRepository = $dataSourceRepository;
+    }
+
+    /**
+     * @Route("/", name="search_index", methods={"GET"})
+     */
+    public function index(Request $request)
+    {
+        $query = $request->query->get('query', '');
+
+        if (!empty($query)) {
+            $matchedDataFlows = $this->searchDataFlows($query);
+            $matchedDataSources = $this->searchDataSources($query);
+        } else {
+            $matchedDataFlows = $this->dataFlowRepository->findAll();
+            $matchedDataSources = $this->dataSourceRepository->findAll();
+        }
+
+
+
+        return $this->render('search/index.html.twig', [
+            'data_flows' => $matchedDataFlows,
+            'data_sources' => $matchedDataSources,
+            'number_of_results' => sizeof($matchedDataFlows) + sizeof($matchedDataSources),
+        ]);
+    }
+
+    private function searchDataFlows(string $search)
+    {
+        $queryBuilder = $this->dataFlowRepository->createQueryBuilder('e');
+
+        $queryBuilder->where('e.name LIKE :search');
+        $queryBuilder->setParameter(':search', $search);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    private function searchDataSources(string $search)
+    {
+        $queryBuilder = $this->dataSourceRepository->createQueryBuilder('e');
+
+        $queryBuilder->where('e.name LIKE :search');
+        $queryBuilder->setParameter(':search', $search);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+}
