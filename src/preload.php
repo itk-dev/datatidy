30c2f0c3e915fc5037aff0a430f4b690ad3f4a46
Mikkel Ricky <rimi@aarhus.dk>
2020-09-07 10:39:41 +0200

    ODAA-220: Updated composer packages

---
+++ src/preload.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is part of itk-dev/datatidy.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+if (file_exists(__DIR__.'/../var/cache/dev/srcApp_KernelDevDebugContainer.preload.php')) {
+    require __DIR__.'/../var/cache/dev/srcApp_KernelDevDebugContainer.preload.php';
+}
